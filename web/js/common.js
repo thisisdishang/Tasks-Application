244b430aee536880bc34e458bfba3cf33ab2df98
thisisdishang <dishangkumarrana@gmail.com>
2025-04-13 18:45:45 +0530

    Update common.js add ajax call script

--- web/js/common.js
+++ web/js/common.js
@@ -2,5 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
  */
+// 
+// ajax call to the server
+// Asynchronous JavaScript and XML
 
+function ajaxCall(method, url, data, isAsync, destination) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById(destination).innerHTML =
+                    this.responseText;
+        }
+    };
+    xhttp.open(method, url, isAsync);
+    xhttp.setRequestHeader('content-type', 'application/x-www-form-urlencoded')
+    xhttp.send(data);
+}
 
