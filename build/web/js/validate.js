e34d2fbe5d2e0ace2ffc661979a68bb9945a5b8c
thisisdishang <dishangkumarrana@gmail.com>
2025-04-21 21:33:35 +0530

    Validation script updated in validate.js

--- build/web/js/validate.js
+++ build/web/js/validate.js
@@ -2,5 +2,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
  */
+//function validate_empty(elementId, labelText) {
+//    var elementValue = document.getElementById(elementId).value;
+//
+//    if (elementValue.trim() === '') {
+//        alert(labelText + " cannot be left empty!");
+//        document.getElementById(elementId).focus();
+//        return false;
+//    }
+//    return true;
+//}
+
+function validate_empty(fieldId, fieldName) {
+    var field = document.getElementById(fieldId);
+
+    if (field && field.value.trim() === "") {
+        alert(fieldName + " cannot be empty!");
+        return false;
+    }
+
+    return true;
+}
 
 
