3a65edbf7874e537176bf58fe06798e0ea969c4b
thisisdishang <dishangkumarrana@gmail.com>
2025-04-26 18:45:45 +0530

    Update TaskRepository Interface add all method

--- src/java/com/finlogic/task/repository/TaskRepository.java
+++ src/java/com/finlogic/task/repository/TaskRepository.java
@@ -4,13 +4,37 @@
  */
 package com.finlogic.task.repository;
 
+import com.finlogic.task.model.Tasks;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
  * @author disha
  */
+
 public interface TaskRepository {
 
-    List users();
+    List getDashboardDetails();
+
+    int insertTask(Tasks tasks);
+
+//    int insertTask(Tasks tasks);
+    
+    List<Map<String, Object>> viewAllTask();
+
+    List<Map<String, Object>> viewTasksByUser(String userId);
+
+    int deleteTask(Tasks task);
+
+    List viewTask(Tasks tasks);
+
+    int updateTask(Tasks task);
+
+    List<Map<String, Object>> getUserDashboardDetails(String userId);
+
+    Tasks getTaskById(String taskId);
+
+    List<Tasks> exportToExcel(String userId);
+
 }
