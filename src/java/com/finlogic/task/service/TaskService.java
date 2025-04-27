a8b462ec92916e8054b3b8e79dab21ee920e211b
thisisdishang <dishangkumarrana@gmail.com>
2025-04-27 18:45:45 +0530

    Update TaskService Interface add all method

--- src/java/com/finlogic/task/service/TaskService.java
+++ src/java/com/finlogic/task/service/TaskService.java
@@ -4,12 +4,46 @@
  */
 package com.finlogic.task.service;
 
+import com.finlogic.task.model.Tasks;
+import com.finlogic.task.model.TasksFormBean;
 import java.util.List;
+import java.util.Map;
+import org.apache.poi.ss.usermodel.Cell;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
  * @author disha
  */
+
 public interface TaskService {
-    List users();
+
+    List getDashboardDetails();
+
+    int insertTask(TasksFormBean tasks);
+
+    List viewAllTask();
+
+    String viewAllTaskGrid();
+
+    String viewTasksByUserGrid(String userId);
+
+    int deleteTask(Tasks task);
+
+    List viewTask(Tasks tasks);
+
+    int updateTask(Tasks tasks);
+
+    List<Map<String, Object>> viewTasksByUser(String userId);
+
+    List<Map<String, Object>> getUserDashboardDetails(String userId);
+
+    Tasks getTaskById(String taskId);
+
+    byte[] exportToExcel(String userId);
+
+    void importFromExcel(MultipartFile file) throws Exception;
+
+    String getStringCellValue(Cell cell);
+
 }
